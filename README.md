b44bf90323f99d33afc838f8f449d4bb32a4caa5
Bastardy <bastard.y@MacBook-Pro.local>
2021-04-18 01:44:48 +0700

    Upload

---
+++ README.md
@@ -0,0 +1,9 @@
+# VELTAI (Vue Electron Taiwind)
+This is the starter kit of web application with Vue + Electron + Tailwind CSS. I made it to myself in the future who are lazy to build it from scratch. Pls enjoy!
+
+
+## Package 
+- Vue v3
+- Electron v11
+- Tailwind v2
+- PostCSS v7
---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class="container mx-auto px-4">
+      <center>
+        <img alt="Vue logo" src="./assets/logo.png">
+        <HelloWorld msg="Welcome to VELTAI App"/>
+      </center>
+    </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
+<style src="./assets/css/tailwind.css">
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
